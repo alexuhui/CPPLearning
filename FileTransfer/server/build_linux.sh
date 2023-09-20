027739816df0e5eedae5a341fa6b8d2d2f0f994a
chenyouhui1 <chenyouhui1@joyy.com>
2023-09-20 09:43:44 +0800

    文件传输

---
+++ FileTransfer/server/build_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+folder_path="./build"
+
+# 检查文件夹是否存在
+if [ ! -d "$folder_path" ]; then
+    echo "${folder_path} 文件夹不存在，正在创建..."
+    mkdir -p "$folder_path"  # 使用mkdir -p以便创建多级目录
+    echo "${folder_path} 文件夹已创建"
+else
+    rm -rf $folder_path/*
+fi
+
+cd $folder_path
+cmake ..
+make
